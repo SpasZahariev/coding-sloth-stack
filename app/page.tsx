5b82e61955ba062b42aa0a31a7711b59204bce5f
Spas Zahariev <spaszahariev54@gmail.com>
2025-10-26 17:11:43 +0100

    working website sso and db integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+'use client'
+
+import { Authenticated, Unauthenticated } from 'convex/react'
+import { SignInButton, UserButton } from '@clerk/nextjs'
+import { useQuery } from 'convex/react'
+import { api } from '../convex/_generated/api'
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click this shadcn button</Button>
-    </div>
-  );
+    <>
+      <Authenticated>
+        <UserButton />
+        <Content />
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
+  )
+}
+
+function Content() {
+  const messages = useQuery(api.messages.getForCurrentUser)
+  return <div>Authenticated content: {messages?.length}</div>
 }
